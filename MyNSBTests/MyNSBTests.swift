34e249cacdf5b67d8387bd7b9b841b1fe3831f6f
Qwerp-Derp <completelyderp@gmail.com>
2017-11-26 18:31:21 +1100

    Added storyboard for timetable

--- MyNSBTests/MyNSBTests.swift
+++ MyNSBTests/MyNSBTests.swift
@@ -10,7 +10,6 @@ import XCTest
 @testable import MyNSB
 
 class MyNSBTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -32,5 +31,4 @@ class MyNSBTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
